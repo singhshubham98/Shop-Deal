8414fc0199e6fa5a47e9e662d283609a92f832af
singhshubham98 <shubhamsingh4204@gmail.com>
2019-08-04 00:26:13 +0530

    user signup and signin setup

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+const jwt = require("jsonwebtoken"); // to generate signed token
+const expressJwt = require("express-jwt"); // for authorization check
+
 exports.signup = (req, res) => {
   const user = new User(req.body);
 
@@ -13,4 +16,39 @@ exports.signup = (req, res) => {
     user.hash_password = undefined;
     res.json({ user });
   });
+};
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  // find the user based on email
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ err: "User with that email does not exist! Please Sign up" });
+    }
+
+    // if user is found make sure that email and password is match
+    // create authenticate method in user model
+    if (!user.authenticate(password)) {
+      return res
+        .status(400)
+        .json({ error: "Email and password do not match!" });
+    }
+    // generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+    // persist the token as 't' in cookie with expiry date
+    res.cookie("t", token, { expire: new Date() + 9999 });
+
+    // return response with user and token to frontend client
+    const { _id, name, email, role } = user;
+
+    return res.json({ token, user: { _id, name, email, role } });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("t");
+  res.json({ message: "Signout Success" });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup } = require("../controllers/user");
+const { signup, signin, signout } = require("../controllers/user");
+const { signupValidator } = require("../validator/validate");
 
-router.post("/signup", signup);
+router.post("/signup", signupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
 
 module.exports = router;
